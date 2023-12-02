5e2762ac0049d624a39d6dbe0bdcfedc903fa485
12 Dansare <dansaren124@outlook.com>
2023-12-02 11:50:06 +0100

    Experiment: audio off for laboratory purposes and made time variables statis

--- src/main/java/org/jakobniinja/Main.java
+++ src/main/java/org/jakobniinja/Main.java
@@ -6,16 +6,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
-    public static final String URL = "https://www.blueclub.se/2193241/13411094-java-performance";
+    private static final String URL = "https://www.blueclub.se/2193241/13433078";
     private static final String PLAY_BUTTON = "//button[@title='Play']";
-
+    private static final int LISTEN_TIME = 130_000;
+    private static final int AWAIT_PAGE_LOAD_TIME = 4_000;
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\dansa\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
-
         // Pass chrome options
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         options.addArguments("--disable-gpu");
@@ -27,14 +26,13 @@ public class Main {
         WebDriver driver = new ChromeDriver(options);
 
         driver.navigate().to(URL);
-
-        Thread.sleep(4000);
+        Thread.sleep(AWAIT_PAGE_LOAD_TIME);
 
         // Click on the Search button
         driver.findElement(By.xpath(PLAY_BUTTON)).click();
 
         // Wait for episodes to finish
-        Thread.sleep(150_000);
+        Thread.sleep(LISTEN_TIME);
 
         driver.quit();
         System.exit(0);
